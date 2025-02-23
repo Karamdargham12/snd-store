0d4399dd59ebd753305d2e9096613d474090b547
Aya-kurbaj <blueb3341@gmail.com>
2025-02-23 13:25:14 +0300

    login

--- client/src/Pages/Auth/Login.js
+++ client/src/Pages/Auth/Login.js
@@ -13,23 +13,22 @@ export default function Login() {
       <div className="md:hidden">
         <MobileNavBar />
       </div>
-
       <div className="flex justify-center">
-        <div className="grid grid-cols-12 w-[95%]  bg-gradient-to-tr from-sky-50 to-sky-500 p-8 pt-14 mt-40 rounded-lg">
-          <div className="col-span-6 col-start-1">
+        <div className="grid lg:grid-cols-12 w-[95%] h-screen lg:h-full  bg-gradient-to-tr from-sky-50 to-sky-500 lg:p-8 p-2 lg:pt-5 pt-0 mt-20 lg:mt-40 rounded-lg">
+          <div className="lg:col-span-6 hidden lg:flex col-span-12 col-start-1">
             <img
               src={loginImg}
-              className="rounded-r-[6rem] w-full h-full "
-              alt="login"
+              className="lg:rounded-r-[6rem] lg:rounded-bl-none rounded-b-[2rem] w-full h-full "
+              alt="Register"
             />
           </div>
-          <form className="col-span-5 col-start-8 pt-10 bg-gray-700/60 rounded-l-[6rem]">
+          <form className="lg:col-span-5 col-span-12 lg:col-start-8 pt-10 mt-3 lg:mt-0 bg-gray-700/60 lg:rounded-l-[6rem] lg:rounded-tr-none rounded-t-xl">
             <div className="w-full flex justify-center items-center">
               <div className="w-3/4">
                 <h1 className="text-4xl text-center text-white font-extrabold">
                   Login
                 </h1>
-               
+              
                 <div className="input-box mt-8 flex items-center border-2 gap-x-2 bg-white">
                   <span className="mdi mdi-email-box text-primaryColor text-xl border-white border-2 pl-2"></span>
                   <input
@@ -45,7 +44,7 @@ export default function Login() {
                     type="password"
                     placeholder="Password"
                   />
-                  </div>
+                </div>
                   <div className="remember-forget pt-8 pl-2 flex justify-between">
                     <div className="flex gap-x-1">
                       <input type="checkbox" className="pl-2" />
