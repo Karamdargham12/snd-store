770a09ab2a38f0d1d18e28201cfcd0aa7de42bbf
Karamdargham12 <112635907+Karamdargham12@users.noreply.github.com>
2025-02-23 11:29:31 +0300

    register

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import {Route, Routes} from 'react-router-dom'
 import React from 'react';
 import HomePage from './Pages/HomePage';
 import './App.css'
-import Login from './Pages/Auth/Login';
+import Login from "./Pages/Auth/Login"
+import Register from './Pages/Auth/Register';
 
 function App() {
   return (
     <Routes>
      <Route path="/" element={<HomePage />} />
-     <Route path="/login" element={<Login/>}/>
+     <Route path="/register" element={<Register />} />
+     <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ client/src/Pages/Auth/Register.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import MobileNavBar from "../../Components/Website/MobileNavBar";
+import NavBar from "../../Components/Website/NavBar";
+import registerImg from "../../images/Register.jpg";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+export default function Register() {
+  const [phone, setPhone] = useState("");
+
+  return (
+    <div className="scroll-bar bg-white">
+      <div className="hidden md:block">
+        <NavBar />
+      </div>
+      <div className="md:hidden">
+        <MobileNavBar />
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-12 w-[95%] h-[90vh]  bg-gradient-to-tr from-sky-50 to-sky-500 p-8 pt-5 mt-40 rounded-lg">
+          <div className="col-span-6 col-start-1">
+            <img
+              src={registerImg}
+              className="rounded-r-[6rem] w-full h-full "
+              alt="Register"
+            />
+          </div>
+          <form className="col-span-5 col-start-8 pt-10 bg-gray-700/60 rounded-l-[6rem]">
+            <div className="w-full flex justify-center items-center">
+              <div className="w-3/4">
+                <h1 className="text-4xl text-center text-white font-extrabold">
+                  Register
+                </h1>
+                <div className="input-box mt-8 flex items-center border-2 gap-x-2 bg-white">
+                  <span className="mdi mdi-account-circle text-primaryColor text-xl border-white border-2 pl-2"></span>
+                  <input
+                    className="border-primaryColor border-10 rounded-md w-full h-9 outline-none"
+                    type="text"
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="input-box mt-8 flex items-center border-2 gap-x-2 bg-white">
+                  <span className="mdi mdi-email-box text-primaryColor text-xl border-white border-2 pl-2"></span>
+                  <input
+                    className="border-primaryColor border-10 rounded-md w-full h-9 outline-none"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="input-box mt-8 flex items-center border-2 gap-x-2 bg-white">
+                  <span className="mdi mdi-lock text-primaryColor text-xl border-white border-2 pl-2"></span>
+                  <input
+                    className="border-primaryColor border-10 rounded-md w-full h-9 outline-none"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="input-box mt-8 flex flex-col">
+                  <PhoneInput
+                    country={"sy"}
+                    value={phone}
+                    placeholder="Phone"
+                    onChange={setPhone}
+                    inputClass="!w-full !h-9 !rounded-md !outline-none "
+                    containerClass="!w-full !flex !items-center"
+                    buttonStyle={{paddingLeft:"3px"}}
+                  />
+                </div>
+                <div className="input-box mt-8 flex items-center border-2 gap-x-2 bg-white">
+                  <span className="mdi mdi-map-marker text-primaryColor text-xl border-white border-2 pl-2"></span>
+                  <input
+                    className="border-primaryColor border-10 rounded-md w-full h-9 outline-none"
+                    type="text"
+                    placeholder="Location"
+                  />
+                </div>
+                <div className="w-full mt-8">
+                  <div className="flex justify-center">
+                    <button className="w-1/2 h-10 bg-primaryColor  rounded-lg text-white font-semibold">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
